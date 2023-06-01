b56c5602104dc204b51e10f91a0b5429830b622a
Hind Hudeib <xxhindsaed27@gmail.com>
2023-06-01 07:37:22 -0700

    update hind pranch 1

--- my-react-app/src/assets/pages/Home.jsx
+++ my-react-app/src/assets/pages/Home.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import HeroSection from "../components/home/HeroSection";
+import SearchBar from "../components/home/SearchBar";
+import FeaturesSection from "../components/home/FeaturesSection";
+import Recommended from "../components/home/Recommended";
+import OffersSection from "../components/home/OffersSection";
+import DropdownNav from "../components/home/DropdownNav";
 
 const Home = () => {
   return (
     <>
-      <HeroSection />
+      <div style={{ position: "relative" }}>
+        <HeroSection />
+      </div>
+      <SearchBar />
+      <FeaturesSection />
+      <Recommended />
+      <OffersSection />
     </>
   );
 };
